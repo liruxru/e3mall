30d0939834139944ddb1b5ca2d0a2cbdafbfe926
liuy <1176881129@qq.com>
2018-10-07 16:07:26 +0800

    添加商品 activeMQ 通知添加索引

--- e3-search/e3-search-service/src/main/java/com/bonc/search/mapper/SearchItemMapper.java
+++ e3-search/e3-search-service/src/main/java/com/bonc/search/mapper/SearchItemMapper.java
@@ -4,12 +4,7 @@ import java.util.List;
 
 import com.bonc.common.pojo.SearchItem;
 
-<<<<<<< HEAD
 public interface SearchItemMapper {
 	List<SearchItem> getItemList();
 	SearchItem getItemById(long id);
-=======
-public interface ItemMapper {
-	List<SearchItem> getItemList();
->>>>>>> branch 'master' of https://github.com/liruxru/e3mall.git
 }
